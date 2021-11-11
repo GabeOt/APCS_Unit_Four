46d43fc80a590afb55e173c3d01a199bdc94e2b1
GabeOt <gabriel.otridge@ssfs.org>
2021-11-11 10:40:50 -0500

    completed the exercises

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,13 +1,28 @@
 public class NestedLoops {
 
     public static String xSquare(int n) {
+        String fin = "";
+        for (int i=1; i<= n; i++) {
+            for (int j=1; j<=n; j++) {
+               fin +="X";
+            }
+            fin +="\n";
+        }
 
-        return "";
+        return fin;
     }
 
     public static String flippedTriangle(int n) {
+        String fin = "";
+        for (int i=n; i>=0; i-- ) {
+            for (int j=0; j<i; j++) {
+                fin += "*";
+            }
+            fin += "\n";
+        }
+        return fin;
 
-        return "";
+
     }
 
     public static String fizzBuzz(int n) {
